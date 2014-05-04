3e962247fce2fb72301aa23da9263bf12de25df6
Matt Andrews <matt@mattandrews.info>
2014-05-05 00:39:59 +0100

    testing

---
+++ fabfile.py
@@ -0,0 +1,27 @@
+from __future__ import with_statement
+from fabric.api import *
+from fabric.contrib.console import confirm
+
+env.hosts = ['matt@162.243.234.162:25000']
+
+def commit():
+    with hide('warnings'):
+        with settings(warn_only=True):
+            msg = prompt("commit message: ")
+            local('git add . && git commit -m "%s"' % msg)
+
+def push():
+    local("git push")
+
+def prepare_deploy():
+    commit()
+    push()
+
+def deploy():
+    prepare_deploy()
+    code_dir = '/var/www/mattandrews/matt'
+    with cd(code_dir):
+        run("git pull")
+        with prefix('source ve/bin/activate'):
+            run("pip install -r requirements.txt")
+        sudo("supervisorctl restart matt")
